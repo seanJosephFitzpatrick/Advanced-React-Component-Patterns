99084182d3dda5c45418cc1d6f27e547d71e1076
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2019-09-10 14:40:52 +0100

    Add Usage component

--- component/Toggle.js
+++ component/Toggle.js
@@ -1,18 +1,30 @@
 import React from "react"
 import {Switch} from "../src/switch"
 
-export default class Toggle extends React.Component {
+class Toggle extends React.Component {
   state = {
     on: false
   }
 
   toggle = () => {
-    this.setState(currentState => {
-      return {on: !currentState.on}
-    })
+    this.setState(
+      currentState => {
+        return {on: !currentState.on}
+      },
+      () => {
+        this.props.onToggle(this.state.on)
+      }
+    )
   }
 
   render() {
     return <Switch on={this.state.on} onClick={this.toggle} />
   }
 }
+
+function Usage({onToggle = (...args) => console.log("onToggle", ...args)}) {
+  return <Toggle onToggle={onToggle} />
+}
+Usage.title = "Build Toggle"
+
+export {Toggle, Usage as default}
