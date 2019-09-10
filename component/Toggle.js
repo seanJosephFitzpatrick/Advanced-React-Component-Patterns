de2436f1479d09f1657f710cce048f14a9ea5181
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2019-09-10 13:18:41 +0100

    Correct setState

--- component/Toggle.js
+++ component/Toggle.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Switch } from "../src/switch";
+import React from "react"
+import {Switch} from "../src/switch"
 
 export default class Toggle extends React.Component {
   state = {
     on: false
-  };
+  }
 
   toggle = () => {
-    this.state({
+    this.setState({
       on: !this.state.on
-    });
-  };
+    })
+  }
 
   render() {
-    return <Switch on={this.state.on} />;
+    return <Switch on={this.state.on} onClick={this.toggle} />
   }
 }
